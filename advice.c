1c3e698f35d09f9950eb0f42ea0f159d5ecb8808
Aleksander Alekseev <afiskon@gmail.com>
2016-07-30 11:42:00 +0300

    First advice!

---
+++ advice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+static const char* advices[] = {
+	"Будь высокомерен!",
+
+};
+
+int main()
+{
+	int idx = 0;
+	printf("%s\n", advices[idx]);
+	return 0;
+}
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gcc advice.c -o advice
