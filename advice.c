b2a3762edfc7628c59b87d646711a5c5258f21ac
afiskon <afiskon@gmail.com>
2016-08-09 18:02:11 +0300

    A new advice from Joe Armstrong - http://joearms.github.io/2016/08/08/Draft-Payment-System-in-Erlang.html

--- advice.c
+++ advice.c
@@ -43,7 +43,8 @@ static const char* advices[] = {
 	"Тише едешь - дальше будешь.",
 	"Не грузи окружающих!",
 	"RTFM",
-	"La gente está muy loca",
+	"La gente está muy loca.",
+	"Performance is not a problem.",
 };
 
 
